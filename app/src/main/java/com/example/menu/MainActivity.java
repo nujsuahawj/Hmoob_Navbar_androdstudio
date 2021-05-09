7b9ef73fe495894ab0ef8466a043c23324c08e52
Nujsua Hawj <nousainther@gmail.com>
2021-05-09 14:57:35 +0700

    first

---
+++ app/src/main/java/com/example/menu/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.menu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+       getMenuInflater().inflate(R.menu.menu_top,menu);
+       return true;
+    }
+}
